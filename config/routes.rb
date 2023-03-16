0592ebc610dbda5b1f21f6680ce83906ffef51cf
Udit <uthakkar@gammastack.com>
2023-03-16 18:16:43 +0530

    authentication done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users
   root 'main#index'
+  get 'home/index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+    
+  }
+
   get 'main/show', to: "main#show"
   get 'home/index', to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
