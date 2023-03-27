c7d970826e75fd98b3204f10c27f43356d8ad9ab
UditThakkar29 <uthakkar@gammastack.com>
2023-03-27 18:17:44 +0530

    adding validations to models

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,6 +3,8 @@ class Board < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  validates :name, presence: true, uniqueness: true
+
   belongs_to :project
   has_many :sprints, dependent: :destroy
 end
--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -3,6 +3,11 @@ class Sprint < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  validates :name, presence: true, uniqueness: true
+  validates :start_time, presence: true
+  validates :start_time, presence: true
+  validates :goal, presence: true
+
   belongs_to :board
   has_many :sprint_tickets, dependent: :destroy
   has_many :tickets, through: :sprint_tickets
