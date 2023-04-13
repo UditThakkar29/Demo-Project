75b4d18ac5d8c581c5c8bb69c836ab1fba77100d
UditThakkar29 <uthakkar@gammastack.com>
2023-04-13 19:43:54 +0530

    feature to login with username and phone number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_053200) do
+ActiveRecord::Schema.define(version: 2023_04_13_123554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,6 +174,8 @@ ActiveRecord::Schema.define(version: 2023_04_12_053200) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "city"
     t.string "country"
+    t.string "phone"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
