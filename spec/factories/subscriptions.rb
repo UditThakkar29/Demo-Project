49de8d3043d82baa8f60a37427cc181082b20933
UditThakkar29 <uthakkar@gammastack.com>
2023-04-14 13:21:17 +0530

    updated user specs aand factories

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :subscription do
-    subscription_status { "MyString" }
+    subscription_status { "active" }
     subscription_end_date { "2023-04-11 16:19:16" }
     subsciption_start_date { "2023-04-11 16:19:16" }
     user { nil }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
+    username { Faker::Internet.username }
+
     email { Faker::Internet.email }
-    password {"password1"}
-    password_confirmation {"password1"}
+    password { Faker::Internet.password }
 
     after(:create) {|user| user.add_role(:user)}
 
